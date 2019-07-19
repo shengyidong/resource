10b690afc056a086891f6b7a0589e76678b1b6b5
syd <dnnlyqai@163.com>
2019-07-19 20:49:05 +0800

    eurekaserver

---
+++ microservices/eversion/microservices-server/src/main/java/com/yidong/microservices/server/microservicesserver/MicroservicesServerApplication.java
@@ -0,0 +1,13 @@
+package com.yidong.microservices.server.microservicesserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesServerApplication.class, args);
+	}
+
+}
---
+++ microservices/eversion/microservices-server/src/test/java/com/yidong/microservices/server/microservicesserver/MicroservicesServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.yidong.microservices.server.microservicesserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MicroservicesServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

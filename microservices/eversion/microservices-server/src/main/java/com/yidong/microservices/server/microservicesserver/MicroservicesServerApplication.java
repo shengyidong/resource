eeaf93c08d39fcd28a061f978c61051239296228
syd <dnnlyqai@163.com>
2019-07-19 22:52:17 +0800

    eurekaserver

--- microservices/eversion/microservices-server/src/main/java/com/yidong/microservices/server/microservicesserver/MicroservicesServerApplication.java
+++ microservices/eversion/microservices-server/src/main/java/com/yidong/microservices/server/microservicesserver/MicroservicesServerApplication.java
@@ -2,8 +2,10 @@ package com.yidong.microservices.server.microservicesserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class MicroservicesServerApplication {
 
 	public static void main(String[] args) {
